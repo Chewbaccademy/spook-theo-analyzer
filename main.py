18c85fa30103abbb118ab4a372a3512dd5398396
Anthony <quereantho@gmail.com>
2021-05-21 10:12:53 +0200

    add multiprocessing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from multiprocessing import Pool
+
+from tqdm import tqdm
 import JSONParser
 from Dataset import Dataset
 import time
@@ -5,6 +8,22 @@ import time
 DATASET_LENGTH = 5
 RESULTS_PER_HOUR = 6
 RECORDS_LENGTH = 10011
+POOL_SIZE = 5
+
+
+def f(index):
+    # print(f'\rTest : {Compter.get_instance().cpt} / {iter_count}', end="")
+    subject = Dataset(records[index:index + DATASET_LENGTH])
+
+    ref = Dataset(records[:index] + records[index + DATASET_LENGTH:])
+
+    nearest = subject.getNearestDataset(ref)
+
+    n_rain = ref[nearest[1] + RESULTS_PER_HOUR].rain
+    r_rain = records[index + (DATASET_LENGTH - 1) + RESULTS_PER_HOUR].rain
+
+    return n_rain and r_rain and (n_rain > 0 and r_rain > 0) or (n_rain == 0 and r_rain == 0)
+
 
 if __name__ == "__main__":
     print("Load dataset...")
@@ -15,37 +34,17 @@ if __name__ == "__main__":
     t = 0
 
     iter_count = records_length - DATASET_LENGTH - RESULTS_PER_HOUR
-    success_count = 0
+    pool = Pool(POOL_SIZE)
 
     print(f'Started at : {time.strftime("%X")}')
     print("\rStarting...")
-    for i in range(iter_count):
-        print(f'\rTest : {i} / {iter_count}', end="")
-        subject = Dataset(records[i:i + DATASET_LENGTH])
-
-        ref = Dataset(records[:i] + records[i + DATASET_LENGTH:])
-
-        nearest = subject.getNearestDataset(ref)
-
-        n_rain = ref[nearest[1] + RESULTS_PER_HOUR].rain
-        r_rain = records[i + (DATASET_LENGTH - 1) + RESULTS_PER_HOUR].rain
-
-        if n_rain and r_rain and (n_rain > 0 and r_rain > 0) or (n_rain == 0 and r_rain == 0):
-            success_count += 1
+    # with pool:
+    result_list_tqdm = []
+    # results = pool.map(f, range(iter_count))
+    success_count = 0
+    for i in tqdm(pool.imap_unordered(f, range(iter_count)), total=iter_count):
+        success_count += 1 if i  else 0
 
     print(f'\nEnded at : {time.strftime("%X")}')
     print(f'Success count = {success_count}')
     print(f'Result = {(success_count * 100) / iter_count}%')
-
-    # random.seed()
-    #
-    # registeredDataset = Dataset.createFromJson(JSONParser.readFile("./registeredData.json"))
-    # newDataset = Dataset.createFromJson1(JSONParser.readFile1("./entry.json"))
-    #
-    # Datatest1 = Dataset([
-    #     newDataset.datas[5],
-    #     newDataset.datas[6],
-    #     newDataset.datas[7],
-    # ])
-    #
-    # print(Dataset(Datatest1.getNearestDataset(registeredDataset)))
